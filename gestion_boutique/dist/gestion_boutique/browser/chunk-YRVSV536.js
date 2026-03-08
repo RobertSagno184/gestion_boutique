fcd94e2b4cf17696d0d43f01819bc5e5830f8168
Robert SAGNO <robertsagno184@.com>
2026-03-08 12:54:29 +0000

    nouvelle mise à jour

---
+++ gestion_boutique/dist/gestion_boutique/browser/chunk-YRVSV536.js
@@ -0,0 +1,1 @@
+var e=[{value:"cash",label:"Esp\xE8ces",icon:"fa-money-bill-wave"},{value:"mobile",label:"Mobile Money",icon:"fa-mobile-screen"},{value:"bank_transfer",label:"Virement bancaire",icon:"fa-building-columns"},{value:"card",label:"Carte bancaire",icon:"fa-credit-card"}];export{e as a};
